71c4a08f998b716925d0c1473cc3376644a3a4bf
Shahzeb <shahzebansari2016@gmail.com>
2019-07-10 17:38:44 +0530

    Third commit!!

--- app/src/main/java/com/example/shahzeb/chainreaction/MainActivity.java
+++ app/src/main/java/com/example/shahzeb/chainreaction/MainActivity.java
@@ -357,7 +357,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("App Name");
+
 
 
         gridLayout=(GridLayout)findViewById(R.id.gridLayout);
